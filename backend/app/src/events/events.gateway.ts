50233d2ba2f88e7e11fcd0ee1b9bdf810192e74b
MuNeNICK <mune0127@munenick.me>
2023-01-08 16:19:35 +0000

    20230109

--- backend/app/src/events/events.gateway.ts
+++ backend/app/src/events/events.gateway.ts
@@ -23,28 +23,21 @@ export class EventsGateway {
   server: Server;
 
   handleConnection(client) {
-    client.on('join', (room) => {
+    client.on('join', async (room) => {
       client.join(room)
     });
+
   }
 
   @SubscribeMessage('message')
   async message(@MessageBody() receiveData: CreateOneThreadsArgs ): Promise<Threads | null> {    
     console.log(receiveData)
-    const sender = receiveData['sender'];
-    const message = receiveData['message'];
-    const date = receiveData['date'];
     const roomname = receiveData['roomname'];
 
     this.server.to(roomname).emit('message', receiveData)
 
     return await this.prisma.threads.create({
-      data: {
-        sender: sender,
-        message: message,
-        date: date,
-        roomname: roomname,
-      },
+      data: receiveData,
     });
   }
 
