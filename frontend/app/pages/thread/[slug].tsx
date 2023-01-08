b1b04bd2b632d7b0f8f3f6fc877a8a0efe3760ee
MuNeNICK <mune0127@munenick.me>
2023-01-08 11:43:10 +0000

    20230108

---
+++ frontend/app/pages/thread/[slug].tsx
@@ -0,0 +1,92 @@
+import dynamic from 'next/dynamic'
+import ErrorPage from 'next/error'
+import { useRouter } from 'next/router'
+import { parseCookies } from 'nookies'
+import { useEffect, useState } from "react"
+import { getPostBySlug } from '../../lib/api'
+import { CMS_NAME } from '../../lib/constants'
+import type PostType from '../../interfaces/post'
+
+const Container = dynamic(() => import("../../components/container"))
+const PostBody = dynamic(() => import("../../components/post-body"))
+const PostHeader = dynamic(() => import("../../components/post-header"))
+const Layout = dynamic(() => import("../../components/layout"))
+const PostTitle = dynamic(() => import("../../components/post-title"))
+const Edit = dynamic(() => import("../../components/post-edit"))
+
+type Props = {
+  post: PostType
+  morePosts: PostType[]
+  preview?: boolean
+}
+
+
+export default function Post({ post, preview }: Props) {
+  const accessToken = parseCookies().accessToken;
+  const [isClient, setIsClient] = useState(false);
+  const editor = () => {
+    if (isClient) {
+      return <Edit />;
+    } else {
+      return null;
+    }
+  };
+
+  if (accessToken) {
+    useEffect(() => {
+      setIsClient(true);
+    }, []);
+  }
+
+  const router = useRouter()
+  if (!router.isFallback && !post?.slug) {
+    return <ErrorPage statusCode={404} />
+  }
+
+  return (
+    <Layout preview={preview}>
+      <Container>
+        {router.isFallback ? (
+          <PostTitle>Loading…</PostTitle>
+        ) : (
+          <>
+            <article className="prose prose-lg max-w-none">
+              <PostHeader
+                displayTitle={post.displayTitle}
+                coverImage={`${process.env.NEXT_PUBLIC_FRONTEND_URL}/api/ogp?title=${post.displayTitle}&height=630&width=1200`}
+                date={post.date}
+                author={post.author}
+              />
+              <PostBody content={post.content} slug={post.slug} />
+            </article>
+          </>
+        )}
+        {editor()}
+      </Container>
+    </Layout>
+  )
+}
+
+type Params = {
+  params: {
+    slug: string
+  }
+}
+
+export async function getServerSideProps({ params }: Params) {
+  const post = await getPostBySlug(params.slug, [
+    'title',
+    'displayTitle',
+    'date',
+    'slug',
+    'author',
+    'content',
+    'coverImage',
+  ])
+
+  return {
+    props: {
+      post,
+    },
+  }
+}
--- frontend/app/pages/thread/index.tsx
+++ frontend/app/pages/thread/index.tsx
@@ -1,19 +1,19 @@
-import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
-import { getAllPosts } from '../lib/api'
+import Container from '../../components/container'
+import MoreStories from '../../components/more-stories'
+import HeroPost from '../../components/hero-post'
+import Intro from '../../components/intro-thread'
+import Layout from '../../components/layout'
+import { getAllPosts } from '../../lib/api'
 import Head from 'next/head'
-import { CMS_NAME } from '../lib/constants'
-import Post from '../interfaces/post'
+import { CMS_NAME } from '../../lib/constants'
+import Post from '../../interfaces/post'
 
 type Props = {
   allPosts: Post[]
 }
 
 export default function Index({ allPosts }: Props) {
-  const deltype = ['thread']
+  const deltype = ['blog']
   const delpost = allPosts.filter((post) => !deltype.includes(post.type))
   const morePosts = delpost.slice(0)
   return (
