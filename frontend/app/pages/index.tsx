fe080274bfe55bb89e4a6f2a3ba2140a054ad8cf
MuNeNICK <mune0127@munenick.me>
2023-01-08 11:29:25 +0000

    20230108

--- frontend/app/pages/index.tsx
+++ frontend/app/pages/index.tsx
@@ -13,8 +13,10 @@ type Props = {
 }
 
 export default function Index({ allPosts }: Props) {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
+  const deltype = ['thread']
+  const delpost = allPosts.filter((post) => !deltype.includes(post.type))
+  const heroPost = delpost[0]
+  const morePosts = delpost.slice(1)
   return (
     <>
       <Layout>
@@ -50,6 +52,7 @@ export async function getServerSideProps() {
     'coverImage',
     'excerpt',
     'displayTitle',
+    'type',
   ])
 
   return {
