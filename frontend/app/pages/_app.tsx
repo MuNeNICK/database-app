d29d30d43d7bea667548b507d62a1b9e6fba507b
MuNeNICK <mune0127@munenick.me>
2023-01-10 01:35:53 +0000

    20230110

--- frontend/app/pages/_app.tsx
+++ frontend/app/pages/_app.tsx
@@ -1,5 +1,7 @@
 import { AppProps } from 'next/app'
 import '../styles/index.css'
+import '../styles/prism.css'
+import "../styles/prism-theme-night-owl.css"
 import { initializeApollo } from '../lib/graphqlClient'
 import { ApolloProvider } from '@apollo/client';
 
